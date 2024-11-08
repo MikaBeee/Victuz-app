ba1279a30610c83881103243c15e07fc57972087
sven <svencl20@gmail.com>
2024-11-08 21:03:25 +0100

    niet ingelogde gebruiken kan aanmelden voor activiteit

---
+++ Victuz/Migrations/20241108184226_AddedIsMember.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Victuz.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedIsMember : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMember",
+                table: "users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "IsMember",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "users",
+                keyColumn: "UserId",
+                keyValue: 2,
+                column: "IsMember",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "users",
+                keyColumn: "UserId",
+                keyValue: 3,
+                column: "IsMember",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "users",
+                keyColumn: "UserId",
+                keyValue: 4,
+                column: "IsMember",
+                value: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsMember",
+                table: "users");
+        }
+    }
+}
